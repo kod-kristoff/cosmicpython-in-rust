145d8600b041e8714455baee846ebc7cef641179
Kristoffer Andersson <kristoffer.andersson@gu.se>
2021-06-16 14:43:55 +0200

    feat: expand domain

--- libs/domain/src/model.rs
+++ libs/domain/src/model.rs
@@ -1,25 +1,55 @@
+use std::collections;
+
+#[derive(Debug)]
 pub struct Batch {
     reference: String,
     sku: String,
     eta: chrono::Date<chrono::Utc>,
-    available_quantity: u32,
+    purchased_quantity: u32,
+    allocations: collections::HashSet<OrderLine>,
 }
 
 impl Batch {
     pub fn new(reference: String, sku: String, qty: u32, eta: chrono::Date<chrono::Utc>) -> Self {
-        Self { reference, sku, eta, available_quantity: qty }
+        let allocations = collections::HashSet::new();
+        Self {
+            reference,
+            sku,
+            eta,
+            purchased_quantity: qty,
+            allocations,
+        }
     }
 
     pub fn can_allocate(&self, line: &OrderLine) -> bool {
-        self.sku == line.sku &&
-        self.available_quantity >= line.qty
+        self.sku == line.sku && self.available_quantity() >= line.qty
     }
 
     pub fn allocate(&mut self, line: OrderLine) {
-       self.available_quantity -= line.qty;
+        if self.can_allocate(&line) {
+            self.allocations.insert(line);
+        }
+        //    self.available_quantity -= line.qty;
+    }
+
+    pub fn available_quantity(&self) -> u32 {
+        self.purchased_quantity - self.allocated_quantity()
+    }
+
+    pub fn allocated_quantity(&self) -> u32 {
+        self.allocations
+            .iter()
+            .fold(0u32, |sum, line| sum + line.qty)
+    }
+
+    pub fn deallocate(&mut self, line: OrderLine) {
+        if self.allocations.contains(&line) {
+            self.allocations.remove(&line);
+        }
     }
 }
 
+#[derive(Eq, Hash, PartialEq, Debug, Clone)]
 pub struct OrderLine {
     orderid: String,
     sku: String,
@@ -38,21 +68,11 @@ mod tests {
 
     #[test]
     fn allocating_to_a_batch_reduces_the_available_quantity() {
-        let mut batch = Batch {
-            reference: "batch-001".to_owned(),
-            sku: "SMALL-TABLE".to_owned(),
-            eta: chrono::Utc::today(),
-            available_quantity: 20,
-        };
-        let line = OrderLine {
-            orderid: "order-ref".to_owned(),
-            sku: "SMALL-TABLE".to_owned(),
-            qty: 2,
-        };
+        let (mut batch, line) = make_batch_and_line("SMALL-TABLE", 20, 2);
 
         batch.allocate(line);
 
-        assert_eq!(batch.available_quantity, 18);
+        assert_eq!(batch.available_quantity(), 18);
     }
 
     fn make_batch_and_line(sku: &str, batch_qty: u32, line_qty: u32) -> (Batch, OrderLine) {
@@ -61,11 +81,9 @@ mod tests {
                 "batch-001".to_owned(),
                 sku.to_owned(),
                 batch_qty,
-                chrono::Utc::today()),
-            OrderLine::new(
-                "order-123".to_owned(),
-                sku.to_owned(),
-                line_qty),
+                chrono::Utc::today(),
+            ),
+            OrderLine::new("order-123".to_owned(), sku.to_owned(), line_qty),
         )
     }
 
@@ -93,11 +111,25 @@ mod tests {
             "batch-001".to_owned(),
             "UNCOMFORTABLE-CHAIR".to_owned(),
             100,
-            chrono::Utc::today());
-        let different_sku_line = OrderLine::new(
-            "order-123".to_owned(),
-            "EXPENSIVE-TOASTER".to_owned(),
-            10);
+            chrono::Utc::today(),
+        );
+        let different_sku_line =
+            OrderLine::new("order-123".to_owned(), "EXPENSIVE-TOASTER".to_owned(), 10);
         assert!(!batch.can_allocate(&different_sku_line));
+    }
+
+    #[test]
+    fn can_only_deallocate_allocated_lines() {
+        let (mut batch, unallocated_line) = make_batch_and_line("DECORATIVE-TRINKET", 20, 2);
+        batch.deallocate(unallocated_line);
+        assert_eq!(batch.available_quantity(), 20);
+    }
+
+    #[test]
+    fn allocation_is_idempotent() {
+        let (mut batch, line) = make_batch_and_line("ANGULAR-DESK", 20, 2);
+        batch.allocate(line.clone());
+        batch.allocate(line);
+        assert_eq!(batch.available_quantity(), 18)
     }
 }
